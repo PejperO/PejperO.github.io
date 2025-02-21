f47a7a6e9ee02be3f0f044f4fb4fac04ff86b540
Mikołaj Polecki <miki.polecki@gmail.com>
2025-02-21 01:36:09 +0100

    dynamic language change part 2

---
+++ language_contact.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const languageToggle = document.getElementById("language-toggle");
+    const langImg = languageToggle.querySelector("img");
+
+    let lang = localStorage.getItem("lang") || "en";
+
+    function updateLanguage() {
+        if (lang === "pl") {
+            document.documentElement.lang = "pl";
+            document.getElementById("home").innerText = "Start";
+            document.getElementById("about-link").innerText = "O mnie";
+            document.getElementById("portfolio").innerText = "Prace";
+            document.getElementById("contact").innerText = "Kontakt";
+            document.getElementById("hello").innerHTML = 'If you\'d like, <span class="name">let\'s chat :)</span>';
+            document.getElementById("footer-text").innerText = "Copyright © 2025 Mikołaj Polecki. Wszelkie prawa zastrzeżone.";
+
+            langImg.src = "images/languages/en.png";
+            langImg.alt = "Change language to English";
+        } else {
+            document.documentElement.lang = "en";
+            document.getElementById("home").innerText = "Home";
+            document.getElementById("about-link").innerText = "About Me";
+            document.getElementById("portfolio").innerText = "Portfolio";
+            document.getElementById("contact").innerText = "Contact";
+            document.getElementById("hello").innerHTML = 'Jeśli chcesz <span class="name">pogadajmy :)</span>';
+            document.getElementById("footer-text").innerText = "Copyright © 2025 Mikołaj Polecki. All rights reserved.";
+
+            langImg.src = "images/languages/pl.png";
+            langImg.alt = "Zmień język na polski";
+        }
+    }
+
+    languageToggle.addEventListener("click", function() {
+        lang = lang === "en" ? "pl" : "en";
+        localStorage.setItem("lang", lang);
+        updateLanguage();
+    });
+
+    updateLanguage();
+});
